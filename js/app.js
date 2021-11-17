b23c5691cb48104aed4fdf6022e52634670050ff
tunjiakanbi <tunjiakanbi@gmail.com>
2021-11-17 13:35:50 -0600

    minor tweaks

--- js/app.js
+++ js/app.js
@@ -18,11 +18,11 @@ $(document).ready(function () {
   navLinks.forEach((link) => {
     link.addEventListener("mouseleave", () => {
       mouseCursor.classList.remove("link-grow");
-      link.classList.remove("hovered-link");
+      // link.classList.remove("hovered-link");
     });
     link.addEventListener("mouseover", () => {
       mouseCursor.classList.add("link-grow");
-      link.classList.add("hovered-link");
+      // link.classList.add("hovered-link");
     });
   });
   //1.   gsap.to('.jumbotron', {x:2000});
